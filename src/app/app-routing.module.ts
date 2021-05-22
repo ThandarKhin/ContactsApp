6b0bd5f6c2338da7bd68e28f62614e51e699904a
Thandar Khin <thandarkhin.gwt@gmail.com>
2021-05-22 19:35:24 +0630

    generate new component, contacts and make route to it.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactsComponent } from './layout/contacts/contacts.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: 'contacts', pathMatch: 'prefix' },
+  { path: 'contacts', component: ContactsComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [ContactsComponent]
 })
 export class AppRoutingModule { }
